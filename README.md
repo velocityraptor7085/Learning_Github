2132d4552aa3dcce9a1ed6a36136ea1bc599fadb
velocityraptor7085 <drarchitch@gmail.com>
2024-01-06 20:05:27 +0530

    commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # THIS IS A HEADING!
 
 This is text that describes the project and the organization of its code , etc.....
+
+
+## this is a subheading.
+More text.
---
+++ program.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("This is a C program.\n");
+    return 0;
+}
+
+/// Git status; add . or add (files list) ; commit ; push
